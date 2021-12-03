cf2ce169d194dcb453ab000448bb46671719e3d8
Alasdair Smith <ali@alasdairsmith.co.uk>
2021-12-03 23:09:01 +0000

    Load bookmarks from db

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,38 @@
+import { useLoaderData } from "remix"
+import type { LoaderFunction } from "remix"
+
+import { db } from "~/utils/db.server"
+
+type LoaderData = {
+  bookmarks: {
+    url: string
+    description: string
+  }[]
+}
+
+export let loader: LoaderFunction = async () => {
+  let bookmarks = await db.bookmark.findMany({
+    select: {
+      url: true,
+      description: true
+    }
+  })
+  return { bookmarks }
+}
+
 export default function Index() {
+  const { bookmarks } = useLoaderData<LoaderData>()
+
   return (
-    <>
-      <p>Some bookmarks here.</p>
-    </>
+    <article>
+      {bookmarks.map(({ url, description }) => {
+        return (
+          <div>
+            <a href={url}>{url}</a>
+            <p>{description}</p>
+          </div>
+        )
+      })}
+    </article>
   )
 }
