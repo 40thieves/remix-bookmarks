df24610d6c6d1405c9b3ae0803b26c5b551d2cdd
Alasdair Smith <ali@alasdairsmith.co.uk>
2021-12-05 21:56:01 +0000

    Calculate relative time ago

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,20 +1,29 @@
-import { useLoaderData } from "remix"
+import { Link, useLoaderData } from "remix"
 import type { LoaderFunction } from "remix"
+import { Temporal } from "@js-temporal/polyfill"
 
 import { db } from "~/utils/db.server"
 
+const relativeTimeFormat = new Intl.RelativeTimeFormat("en", {
+  numeric: "auto"
+})
+
 type LoaderData = {
-  bookmarks: {
+  bookmarks: Array<{
+    id: number
     url: string
     description: string
-  }[]
+    createdAt: string // Not Date, since it is JSON-stringified
+  }>
 }
 
 export let loader: LoaderFunction = async () => {
   let bookmarks = await db.bookmark.findMany({
     select: {
+      id: true,
       url: true,
-      description: true
+      description: true,
+      createdAt: true
     }
   })
   return { bookmarks }
@@ -25,14 +34,24 @@ export default function Index() {
 
   return (
     <article>
-      {bookmarks.map(({ url, description }) => {
+      {bookmarks.map(({ id, url, description, createdAt }) => {
         return (
-          <div>
+          <div key={id}>
             <a href={url}>{url}</a>
             <p>{description}</p>
+            <Link to={String(id)}>{daysAgo(createdAt)}</Link>
           </div>
         )
       })}
     </article>
   )
+}
+
+function daysAgo(timestamp: string) {
+  let date = Temporal.Instant.from(timestamp)
+  let duration = date
+    .since(Temporal.Now.instant())
+    .round({ largestUnit: "day", smallestUnit: "day" })
+
+  return relativeTimeFormat.format(duration.days, "day")
 }
