a5c3a912284d1c6d5cbe2cf1b85662703490c27c
Alasdair Smith <ali@alasdairsmith.co.uk>
2021-12-07 23:02:33 +0000

    Add dates to seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -16,26 +16,34 @@ seed()
 function getBookmarks() {
   return [
     {
-      url: "https://alasdairsmith.co.uk",
-      title: "Alasdair Smith | 40thieves",
-      description: "My website"
-    },
-    {
       url: "https://example.com",
       title: "Example Domain",
-      description: "Example website"
+      description: "Example website",
+      createdAt: new Date("1989-03-12T09:56:00Z"),
+      updatedAt: new Date("1989-03-12T09:56:00Z")
     },
     {
-      url: "https://remix.run",
-      title: "Remix - Build Better Websites",
-      description: "Remix homepage"
+      url: "https://alasdairsmith.co.uk",
+      title: "Alasdair Smith | 40thieves",
+      description: "My website",
+      createdAt: new Date("2021-01-01T00:00:00Z"),
+      updatedAt: new Date("2021-01-01T00:00:00Z")
     },
     {
       url: "https://piccalil.li/quick-tip/use-css-clamp-to-create-a-more-flexible-wrapper-utility",
       title:
         "Use CSS Clamp to create a more flexible wrapper utility - Quick Tip - Piccalilli",
       description:
-        "Tip to use the CSS clamp function to make text wrap more nicely in responsive designs"
+        "Tip to use the CSS clamp function to make text wrap more nicely in responsive designs",
+      createdAt: new Date("2021-03-16T21:45:00Z"),
+      updatedAt: new Date("2021-03-16T21:45:00Z")
+    },
+    {
+      url: "https://remix.run",
+      title: "Remix - Build Better Websites",
+      description: "Remix homepage",
+      createdAt: new Date("2021-11-22T20:47:00Z"),
+      updatedAt: new Date("2021-11-22T20:47:00Z")
     }
   ]
 }
