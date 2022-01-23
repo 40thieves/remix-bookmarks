8363ddbbc4a0cebd9f1c4bd694384b372012ef00
Alasdair Smith <ali@alasdairsmith.co.uk>
2022-01-23 22:30:09 +0000

    Pagniate list

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,18 +4,9 @@ import { Temporal } from "@js-temporal/polyfill"
 import { Bookmark } from "@prisma/client"
 
 import { db, JsonifyModel } from "~/utils/db.server"
+import { badRequest } from "~/utils/http-response"
 
 import stylesUrl from "~/styles/list.css"
-
-const relativeTimeFormat = new Intl.RelativeTimeFormat("en", {
-  numeric: "auto"
-})
-
-type LoaderData = {
-  bookmarks: JsonifyModel<
-    Pick<Bookmark, "id" | "url" | "description" | "createdAt">
-  >[]
-}
 
 export let links: LinksFunction = () => {
   return [
@@ -26,21 +17,61 @@ export let links: LinksFunction = () => {
   ]
 }
 
-export let loader: LoaderFunction = async () => {
-  let bookmarks = await db.bookmark.findMany({
-    select: {
-      id: true,
-      url: true,
-      description: true,
-      createdAt: true
-    },
-    orderBy: { createdAt: "desc" }
-  })
-  return { bookmarks }
+type LoaderData = {
+  bookmarks: JsonifyModel<
+    Pick<Bookmark, "id" | "url" | "description" | "createdAt">
+  >[]
+  pagination: {
+    current: number
+    hasPrevious: boolean
+    previous: number
+    hasNext: boolean
+    next: number
+  }
+}
+
+const PER_PAGE = 20
+
+export let loader: LoaderFunction = async ({ request }) => {
+  let searchParams = new URL(request.url).searchParams
+  let page = parseInt(searchParams.get("page") || "1", 10)
+
+  if (page < 1) throw badRequest({ error: { formError: ["invalid_page"] } })
+
+  // Offset is zero-indexed page number multiplied by the number of bookmarks per page
+  let skip = (page - 1) * PER_PAGE
+
+  let [bookmarks, totalCount] = await db.$transaction([
+    db.bookmark.findMany({
+      select: {
+        id: true,
+        url: true,
+        description: true,
+        createdAt: true
+      },
+      orderBy: { createdAt: "desc" },
+      skip,
+      take: PER_PAGE
+    }),
+    db.bookmark.count()
+  ])
+
+  return {
+    bookmarks,
+    pagination: {
+      current: page,
+      // If we're on the first page, there is no previous page
+      hasPrevious: page - 1 > 0,
+      previous: page - 1,
+      // If the next offset is beyond the total count, there is no next page
+      hasNext: skip + PER_PAGE <= totalCount,
+      next: page + 1
+    }
+  }
 }
 
 export default function Index() {
-  const { bookmarks } = useLoaderData<LoaderData>()
+  let { bookmarks, pagination } = useLoaderData<LoaderData>()
 
   return (
     <main className="bookmarks__list">
@@ -57,9 +88,21 @@ export default function Index() {
           </div>
         )
       })}
+      <div className="pagination__container">
+        {pagination.hasPrevious && (
+          <Link to={`.?page=${pagination.previous}`}>Previous</Link>
+        )}
+        {pagination.hasNext && (
+          <Link to={`.?page=${pagination.next}`}>Next</Link>
+        )}
+      </div>
     </main>
   )
 }
+
+const relativeTimeFormat = new Intl.RelativeTimeFormat("en", {
+  numeric: "auto"
+})
 
 function daysAgo(timestamp: string) {
   let date = Temporal.Instant.from(timestamp)
