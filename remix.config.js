95567ceee4e688bdc1abfeffe24e40e38ec39442
Alasdair Smith <ali@alasdairsmith.co.uk>
2023-09-07 23:11:41 +0100

    Opt into new v2 flags
    devServerPort has been deprecated but I don't think I'm using it so, just remove it

--- remix.config.js
+++ remix.config.js
@@ -3,12 +3,13 @@
  */
 module.exports = {
   ignoredRouteFiles: [".*"],
-  devServerPort: 8002,
   serverModuleFormat: "cjs",
   future: {
     v2_routeConvention: true,
     v2_meta: true,
     v2_errorBoundary: true,
-    v2_normalizeFormMethod: true
+    v2_normalizeFormMethod: true,
+    v2_headers: true,
+    v2_dev: true
   }
 }
